3dfd235898938e20f2307a9b2dd58609ceb809d4
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-22 21:59:04 +0300

    templating engine

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { initialDatabase } = require('../config/db');
+const { initViewEngine } = require('../config/hbs');
+const { auth } = require('./middlewares/authMiddleware');
+const cookieParser = require('cookie-parser');
+const router = require('./router');
+const app = express();
+const port = 3000;
+
+initViewEngine(app);
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(auth);
+app.use(router);
+
+initialDatabase()
+    .then(() => {
+        console.log("Database initialized successfully!");
+        app.listen(port, () => console.log(`Server is working at: http://localhost:${port}`));
+    })
+    .catch((error) => {
+        console.log("Database error: " + error.message);
+    });
